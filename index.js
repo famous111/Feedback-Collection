e8272a97cfaa1c8c3674acf30351eb2ff669c2ff
Ming Zhu <mingzhu@Mings-MacBook-Air.local>
2021-05-08 19:39:04 -0400

    test Google oauth2.0

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const app = express();
+const keys = require("./config/keys");
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token", accessToken);
+      console.log("refresh token", refreshToken);
+      console.log("profile:", profile);
+    }
+  )
+);
 
-const PORT = process.env.env.PORT || 5000; // use port 5000 at localhost
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
+
+// const PORT = process.env.env.PORT
+const PORT = 5000; // use port 5000 at localhost
 app.listen(PORT);
 // http://localhost:5000
