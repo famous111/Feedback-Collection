d21990741b252ad060049a0dbdd4de817c57ed0b
Ming Zhu <zhumphy@gmail.com>
2021-06-03 15:59:10 -0400

    finish 170 validate email address

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,9 +4,13 @@ import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 import { Link } from "react-router-dom";
 import SurveyField from "./SurveyField";
+import validateEmails from "../../utils/validateEmails";
 
 const FIELDS = [
-  { label: "Survey Title", name: "title" },
+  {
+    label: "Survey Title",
+    name: "title",
+  },
   { label: "Subject Line", name: "subject" },
   { label: "Email Body", name: "body" },
   { label: "Recipient List", name: "emails" },
@@ -46,6 +50,21 @@ class SurveyForm extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  errors.emails = validateEmails(values.emails || "");
+
+  _.each(FIELDS, ({ name }) => {
+    if (!values[name]) {
+      errors[name] = "You must provide a value";
+    }
+  });
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: "surveyForm",
 })(SurveyForm);
