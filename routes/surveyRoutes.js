19a7cebd22b9bfb1520ac6e04815eae2926a49d1
Ming Zhu <zhumphy@gmail.com>
2021-06-05 15:41:04 -0400

    Finish Section 12

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,12 +10,12 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = (app) => {
-  app.get("/api/surveys/thanks", (req, res) => {
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
     res.send("Thanks for voting!");
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
-    const events = _.chain(req.body)
+    _.chain(req.body)
       .map(({ url, email }) => {
         const p = new Path("/api/surveys/:surveyId/:choice");
         const match = p.test(new URL(url).pathname);
@@ -26,14 +26,27 @@ module.exports = (app) => {
             choice: match.choice,
           };
         }
-
-        res.send({});
       })
       .compact()
       .uniqBy("email", "surveyId")
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false },
+            },
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { "recipients.$.responded": true },
+            lastResponded: new Date(),
+          }
+        ).exec();
+      })
       .value();
 
-    console.log(events);
+    res.send({});
   });
 
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
